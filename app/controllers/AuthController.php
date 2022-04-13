9346669768ab76cfec847ea12f01a374fdabee0d
Alex Ortigosa <alexortigosa179@gmail.com>
2022-04-13 14:15:24 +0200

    WritingTest for Users & Posts

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -3,15 +3,21 @@
 class AuthController extends ApplicationController {
 
     public function __construct(){
-        $this->db = new JsonModel();
+        // $this->db = new JsonModel();
     }
-
+    
     public function loginAction(){
+        $this->db = new UserModel();
+        $this->db->writingTest();
+
         $this->view->message = "Hello from auth#login";
         $this->view->setTitle('Login Page');
     }
 
     public function registerAction(){
+        $this->db = new PostModel();
+        $this->db->writingTest();
+        
         $this->view->message = "Hello from auth#register";
         $this->view->setTitle('Register Page');
     }
