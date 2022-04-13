42ff33015d13b50e24253c51ea5b4d89b15a113b
Alex Ortigosa <alexortigosa179@gmail.com>
2022-04-13 17:34:32 +0200

    Added Login logic up to data validation

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,17 +1,26 @@
 <?php 
 
 class AuthController extends ApplicationController {
+    
+    protected $errMsg = '';
 
-    public function __construct(){
-        // $this->db = new JsonModel();
-    }
+    public function __construct(){}
     
     public function loginAction(){
-        $this->db = new UserModel();
-        $this->db->writingTest();
+        $this->view->setTitle('Login | ' . APP_TITLE);
 
-        $this->view->message = "Hello from auth#login";
-        $this->view->setTitle('Login Page');
+        if (!$_POST) return;
+
+        $username = $_POST['username'] ?? '';
+        $password = $_POST['password'] ?? '';
+
+        $result = $this->validate(array('username' => $username, 'password' => $password));
+        $result = array_search('invalid', $result);
+
+        if($result){
+            $_SESSION['error'] = $this->errMsg;
+        }
+
     }
 
     public function registerAction(){
@@ -20,6 +29,33 @@ class AuthController extends ApplicationController {
         
         $this->view->message = "Hello from auth#register";
         $this->view->setTitle('Register Page');
+    }
+
+    /**
+     * Validates multiple fields at once
+     * 
+     * @param array the data to be evaluated
+     * @return array the data with some values being invalid if validation failed for those fields
+     */
+    public function validate(array $data){
+        foreach ($data as $key => $fieldValue) {
+            switch ($key) {
+                case 'username':
+                    if(!preg_match_all('/(?=.*[0-9].*[0-9]){2,}(?=.*[a-zA-Z].*[a-zA-Z]){4,}/', $fieldValue)){
+                        $data[$key] = 'invalid';
+                        $this->errMsg .= '• Usernames must have at least 4 letters and 2 numbers, and should not contain special characters.<br>';
+                    }
+                    break;
+                case 'password':
+                    if(!preg_match_all('/(?=.*[A-Z])(?=.*[-]){6,}/', $fieldValue)){
+                        $data[$key] = 'invalid';
+                        $this->errMsg .= '• Passwords must be at least 6 characters long and contain a hyphen and an uppercase letter.<br>';
+                    }
+                    break;
+            }
+        }
+
+        return $data;
     }
 
 }
