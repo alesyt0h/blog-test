d7634fcd8b6ce961f01b2ea3e1ef0d5083eda3be
Alex Ortigosa <alexortigosa179@gmail.com>
2022-04-13 13:15:28 +0200

    Added parsing of the JSON files and storing on a temp variable

--- lib/base/Model.php
+++ lib/base/Model.php
@@ -1,6 +1,38 @@
 <?php
 
+/**
+ * This class manages the JSON files, writing and parsing
+ */
 class Model
 {
+	protected $_jsonData = [];
+    protected $_users = [];
+    protected $_blogPosts = [];
+
+	protected $dbDir = ROOT_PATH . '/db/';
+
+	public function __construct(){
+        // Parse the DB's
+        $this->parseJSON('posts');
+        $this->parseJSON('users');
+    }
+
+	/**
+     * Parses the given JSON file and stores it on $_jsonData
+     * @param string the file to parse. Do not include '.json' in the filename
+     */
+    public function parseJSON(string $file){
+        if(substr($file, -5) !== '.json'){
+            $file .= '.json';
+        }
+
+        if(!file_exists($this->dbDir . $file)){
+            file_put_contents($this->dbDir . $file, '');
+        }
+
+        $jsonRaw = file_get_contents($this->dbDir . $file);
+        $this->_jsonData = json_decode($jsonRaw, true);
+    }
+
 
 }
