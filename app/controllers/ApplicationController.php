e0c660d46368e9980e323e213152499edb874ff6
Alex Ortigosa <alexortigosa179@gmail.com>
2022-04-13 17:46:27 +0200

    Added a root redirect in ApplicationController

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -6,6 +6,22 @@
  */
 class ApplicationController extends Controller 
 {
+    /**
+     * Redirects the user to the webroot or another path if specified
+     * 
+	 * @param string $path must start with slash. Eg. /blog
+	 * @return void
+	 */
+    protected function redirect(string $path = ''){
+		header('Location: ' . WEB_ROOT . $path);
+        die();
+	}
+
+    /**
+	 * Redirects the user to same URL, mostly to prevent form resubmission
+     * 
+	 * @return void
+	 */
     protected function selfRedirect(){
 		header('Location: ' . WEB_ROOT . substr($_SERVER['REQUEST_URI'], strlen(WEB_ROOT)));
 		die();
